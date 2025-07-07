845a1546a1eb358f8080d272427b7ae858eac4c2
MarkosKs <hp2221203@hua.gr>
2025-07-07 18:45:15 +0300

    +1

---
+++ Είσοδος Έξοδος δεδομένων/Είσοδος Έξοδος δεδομένων/KELYFOS_1.py
@@ -0,0 +1,9 @@
+def card():
+    print('____________________________________')
+    print()
+    print()
+    print('                ' , 'Αντώνης Παπαδόπουλος')
+    print(10*' ', 'antonis.papaadop@@ellak.gr')
+    print(10*' ', '21312441251')
+    print(2*'\n')
+    print(40*'_')
